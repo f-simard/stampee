da77dfc1ee1faba47b4ed8e5c022804797330d22
f-simard <e2396414@cmaisonneuve.qc.ca>
2024-09-18 14:06:31 -0400

    ajuster CSS vue membre/enchere

--- views/enchere/parMembre.php
+++ views/enchere/parMembre.php
@@ -15,14 +15,35 @@
 				<a href="{{base}}/enchere/voir?idEnchere={{enchere.idEnchere}}">
 					<article class="carte-lot" data-mode="liste">
 						<section class="info-lot">
-							<h5>Enchere {{enchere.idEnchere}}</h5>
-							<p>Date debut: {{enchere.dateDebut}}</p>
-							<p>Date fin: {{enchere.dateFin}}</p>
-							<p>Prix plancher: {{enchere.prixPlancher}}</p>
-							<p>Estimation: {{enchere.estimation}}</p>
-							<p>Devise de base: {{enchere.idDevise}}</p>
-							<p>Statut: {{enchere.statut}}</p>
-							<p>Nombre de timbre: {{enchere.nbTimbre}}</p>
+								<h5>Enchere {{enchere.idEnchere}}</h5>
+							<div class="paire">
+								<p>Date debut</p>
+								<p>{{enchere.dateDebut}}</p>
+							</div>
+							<div class="paire">
+								<p>Date fin</p>
+								<p>{{enchere.dateFin}}</p>
+							</div>
+							<div class="paire">
+								<p>Prix plancher</p>
+								<p>{{enchere.prixPlancher}}</p>
+							</div>
+							<div class="paire">
+								<p>Estimation</p>
+								<p>{{enchere.estimation}}</p>
+							</div>
+							<div class="paire">
+								<p>Devise de base</p>
+								<p>{{enchere.idDevise}}</p>
+							</div>
+							<div class="paire">
+								<p>Statut</p>
+								<p>{{enchere.statut}}</p>
+							</div>
+							<div class="paire">
+								<p>Nombre de timbre</p>
+								<p>{{enchere.nbTimbre}}</p>
+							</div>
 						</section>
 						<form action="{{base}}/enchere/supprimer" method="post">
 							<input type="hidden" name="idEnchere" value="{{enchere.idEnchere}}">
